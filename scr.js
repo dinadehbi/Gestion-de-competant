5ad15f80364945ebac0844f0091bf93c050097d8
dinadehbi <147596316+dinadehbi@users.noreply.github.com>
2024-09-10 13:10:17 +0100

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,4 +1,13 @@
-let text = "Repetition is a good thing.";
- for (let i = 0; i <100; i++){
-     console.log(text[i]);
- }
+let numbers = [10,2,0,9,3,22];
+
+let stock;
+for(let i = 0;i<numbers.length;i++){
+    for(let j=i+1;j<numbers.length;j++){
+        if(numbers[i]<numbers[j]){
+          stock =  numbers[i]
+          numbers[i]=numbers[j]
+          numbers[j]= stock  
+        }
+     }
+}
+console.log(numbers);
